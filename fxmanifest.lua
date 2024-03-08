638118c76258417d01b82105be5c0791c52b452e
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 03:23:36 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'HHFW AI Doc'
+
+version '0.1.0'
+
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
+
+shared_script '@es_extended/imports.lua'
